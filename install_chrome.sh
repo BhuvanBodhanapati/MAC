059ffc2f448eb7103d827032cd1e9c721af8358b
Bhuvan Bodhanapati <96065160+BhuvanBodhanapati@users.noreply.github.com>
2024-07-28 23:17:09 +0530

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -6,8 +6,8 @@ set -e
 echo "Downloading and unpacking Chrome..."
 
 # Create a directory for Chrome
-mkdir -p /tmp/chrome
-cd /tmp/chrome
+mkdir -p $HOME/chrome
+cd $HOME/chrome
 
 # Download a pre-built Chrome binary
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -17,10 +17,12 @@ ar x google-chrome-stable_current_amd64.deb
 tar -xvf data.tar.xz
 
 # Move the Chrome binary to a directory included in the PATH
-mkdir -p $HOME/chrome
 mv opt/google/chrome/* $HOME/chrome/
 
 # Clean up
-rm -rf /tmp/chrome
+rm -rf opt google-chrome-stable_current_amd64.deb data.tar.xz
+
+# Verify installation
+$HOME/chrome/google-chrome --version
 
 echo "Chrome installed successfully."
