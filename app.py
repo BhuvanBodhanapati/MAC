ae591ab3ad637bcc03d3c5d64a6c2336d164701d
Bhuvan Bodhanapati <96065160+BhuvanBodhanapati@users.noreply.github.com>
2024-07-28 23:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 import time
 import logging
+import os
 
 # Setup logging
 # logging.basicConfig(level=logging.INFO)
@@ -25,7 +26,7 @@ def mark_attendance():
     chrome_options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
     chrome_options.add_argument('--log-level=3')  # Suppress logging
 
-    chrome_binary_path = os.path.expanduser('~/chrome/google-chrome')
+    chrome_binary_path = os.path.expanduser('~/chrome/opt/google/chrome/google-chrome')
     chrome_options.binary_location = chrome_binary_path
 
 
